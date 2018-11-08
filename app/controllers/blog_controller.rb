deedf41078818bbbdbcbd4af5a2357553c826ef7
danielmoralesp <daniel_moralesp@hotmail.com>
2018-11-07 20:34:37 -0500

    Render y Redirect

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,11 +1,46 @@
 class BlogController < ApplicationController
+  #layout y render
+  #redirect
+
   def index
   end
 
-  def edit
+  def new
+    #get
   end
 
-  def new
+  def create
+    #post
+
+    #transaccion en la base datos
+
+    if condition
+      # si se cumple
+      redirect_to blog_index_path
+    else
+      #si falla
+      render :new
+    end
+  end
+
+  def edit
+    #get
+  end
+
+  def update
+    #post / put / patch
+
+    if condition
+      # si se cumple
+      redirect_to home_about_path
+    else
+      #si falla
+      render :edit
+    end
+  end
+
+  def delete
+    #post /delete / destroy
   end
 
   def show
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,11 +8,14 @@ class HomeController < ApplicationController
   end
 
   def about
+    redirect_to blog_index_path
   end
 
   def contact_us
+    render :prices
   end
 
   def prices
+    redirect_to "https://google.com"
   end
 end
