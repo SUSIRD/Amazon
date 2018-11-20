bc8491368525f5ec7aa5bd938e6151de41d6eb59
danielmoralesp <daniel_moralesp@hotmail.com>
2018-11-19 20:13:43 -0500

    Devise Signin Design

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,4 +16,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( admin.css )
 Rails.application.config.assets.precompile += %w( admin.js )
 
-Rails.application.config.assets.precompile += %w( signin.css )
+Rails.application.config.assets.precompile += %w( signin.scss )
