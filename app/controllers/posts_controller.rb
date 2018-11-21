12835921976bffe7cbad83294cf148df495b015c
danielmoralesp <daniel_moralesp@hotmail.com>
2018-11-20 20:51:31 -0500

    will_paginate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,12 @@ class PostsController < ApplicationController
   before_action :is_admin?, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc).paginate(page: params[:page], per_page: 2)
   end
 
   def show
     @post = Post.find(params[:id])
+    @comments = @post.comments.order(created_at: :desc).paginate(page: params[:page], per_page: 2)
   end
 
   def new
