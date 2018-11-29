3e1281fdac97add6a15c6c7938d9965bb7d1061f
danielmoralesp <daniel_moralesp@hotmail.com>
2018-11-28 19:59:08 -0500

    Final category features

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,7 @@ class Post < ApplicationRecord
   belongs_to :category
   has_many :comments, dependent: :destroy
 
-  validates :title, presence: true
-  validates :content, presence: true
+  validates :title, :content, presence: true
 
   # scope :only_published, -> { where(pubished: true) }
   # scope :popular, -> { where("views > ?", 10) }
---
+++ app/models/subscriptor.rb
@@ -0,0 +1,2 @@
+class Subscriptor < ApplicationRecord
+end
