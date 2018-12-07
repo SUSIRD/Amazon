31c3f5020aa6aefec7a07ddfde5902d15341a87a
danielmoralesp <daniel_moralesp@hotmail.com>
2018-12-06 21:00:28 -0500

    Users on admin panel

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,18 @@
 class AdminController < ApplicationController
+  before_action :authenticate_user!
+  before_action :is_admin?
 
   layout "admin"
 
   def index
+    @users = User.all.order(created_at: :desc)
   end
+
+  private
+    def is_admin?
+      unless current_user.admin?
+        flash[:alert] = "You dont have permissions"
+        redirect_to root_path
+      end
+    end
 end
