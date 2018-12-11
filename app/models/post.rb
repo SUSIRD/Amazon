71a3f01e6db2d615c7e64e1b3f8b22f0f1857eac
danielmoralesp <daniel_moralesp@hotmail.com>
2018-12-11 17:24:36 -0500

    Annotate gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer
+#  title       :string
+#  content     :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :integer
+#
+
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
@@ -20,4 +33,8 @@ class Post < ApplicationRecord
   # def published
   #   self.published == true
   # end
+
+  def self.unique
+
+  end
 end
