233ecb303577e6b1bbb3eb5931c55fb7bc72a4ca
danielmoralesp <daniel_moralesp@hotmail.com>
2018-12-14 19:06:23 -0500

    Product new test

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get products_new_url
+    get new_product_url
     assert_response :success
   end
 
-  test "should get create" do
-    get products_create_url
-    assert_response :success
-  end
+  # test "should get create" do
+  #   get create_products_url
+  #   assert_response :success
+  # end
 
 end
